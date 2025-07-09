9494a0b893525ce34857d4d0c6d482ca0ebd6e93
Chinonye Okorie <nanaokorie@gmail.com>
2025-07-09 21:03:18 +0100

    edited test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders input and button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const input = screen.getByPlaceholderText(/enter city/i);
+  const button = screen.getByText(/get weather/i);
+
+  expect(input).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
 });
